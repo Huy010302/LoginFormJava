d15bc9558ca22c19784b302dc213c284c71aed54
Huy010302 <111289985+Huy010302@users.noreply.github.com>
2024-03-29 05:15:37 +0700

    LoginForm

---
+++ LoginForm/src/main/java/com/mycompany/loginform/LoginForm.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.loginform;
+
+/**
+ *
+ * @author ngleh
+ */
+public class LoginForm {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
